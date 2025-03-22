23a1cdf0f49979f0ab1e82690c8e9273eb00c1f2
Dmitrii Kozyrev <dmkozyrev@rambler.ru>
2025-03-22 04:20:15 +0300

    новый парсер

--- netlify/functions/parse-battle.js
+++ netlify/functions/parse-battle.js
@@ -1,22 +1,28 @@
-// netlify/functions/parse-battle.js
+// .netlify/functions/parse-battle.js
 exports.handler = async (event, context) => {
-  // Динамически импортируем node-fetch и получаем его дефолтный экспорт
+  // Импортируем node-fetch для выполнения HTTP-запроса
   const { default: fetch } = await import('node-fetch');
 
-  // Получаем параметры запроса (warid и show) из query string
-  const { warid, show } = event.queryStringParameters || {};
+  // Получаем параметры запроса: warid и show_for_all
+  const { warid, show_for_all } = event.queryStringParameters || {};
   if (!warid) {
     return {
       statusCode: 400,
       body: "Missing parameter: warid",
     };
   }
-
-  // Формируем URL целевой страницы
-  let url = `https://www.heroeswm.ru/war.php?warid=${warid}`;
-  if (show) {
-    url += `&show=${show}`;
+  if (!show_for_all) {
+    return {
+      statusCode: 400,
+      body: "Missing parameter: show_for_all",
+    };
   }
+
+  // Генерируем случайное число для параметра rand
+  const rand = Math.random();
+
+  // Формируем URL целевой страницы с фиксированными параметрами
+  const url = `https://www.heroeswm.ru/battle.php?warid=${encodeURIComponent(warid)}&lastturn=-2&lastmess=0&lastmess2=0&rand=${rand}&showinsertion=1&pl_id=0&lastdata=1&show_for_all=${encodeURIComponent(show_for_all)}`;
 
   try {
     const res = await fetch(url);
@@ -27,13 +33,14 @@ exports.handler = async (event, context) => {
       };
     }
     let html = await res.text();
+    // Заменяем относительные пути на абсолютные, если необходимо
     html = html.replace(/(href|src)=["']\/(?!\/)/g, '$1="https://www.heroeswm.ru/');
+
     return {
       statusCode: 200,
       headers: {
         "Content-Type": "text/html; charset=utf-8",
-        // Разрешаем кросс-доменный доступ к этой функции
-        "Access-Control-Allow-Origin": "*"
+        "Access-Control-Allow-Origin": "*"  // Разрешаем кросс-доменный доступ
       },
       body: html,
     };
