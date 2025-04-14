a2bf1a445b20bec939f91970729a46f251a72dce
Dmitrii Kozyrev <dmkozyrev@rambler.ru>
2025-04-14 13:47:04 +0300

    Update getBattles.js

--- netlify/functions/getBattles.js
+++ netlify/functions/getBattles.js
@@ -13,7 +13,7 @@ exports.handler = async (event, context) => {
     // Получаем все бои из таблицы "battles"
     const { data: battles, error: battlesError } = await supabase
       .from('battles')
-      .select('*');
+      .select('*').limit(1000000);
     if (battlesError) throw battlesError;
 
     // Получаем значение маркеров из таблицы "settings"
