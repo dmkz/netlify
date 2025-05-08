39bddf4f50c20e0bd6e11760fddc575f29807fb6
Dmitrii Kozyrev <dmkozyrev@rambler.ru>
2025-05-08 17:35:33 +0300

    Update collect.js

--- netlify/functions/collect.js
+++ netlify/functions/collect.js
@@ -14,7 +14,7 @@ const CORS_HEADERS = {
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_SERVICE_KEY
 );
 
 exports.handler = async (event) => {
